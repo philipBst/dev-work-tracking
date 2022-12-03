6c62e25e5fdbabcb11ea8d595a7bac35580d3003
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 12:32:07 +0545

    feat: extend theme for multiselect

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { MultiSelectTheme } from "chakra-multiselect";
 
 import App from "./App";
 
 import "./index.css";
 
+const theme = extendTheme({
+  components: {
+    MultiSelect: MultiSelectTheme,
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
