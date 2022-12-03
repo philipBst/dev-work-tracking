4e3079c4b0d5a093eb051b895e6000bc52ed6774
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 12:22:20 +0545

    feat: create component filter form

--- client/src/components/FilterForm.tsx
+++ client/src/components/FilterForm.tsx
@@ -1,3 +1,32 @@
-export const FilterForm = () => <>Filter form</>;
+import { Button, HStack, Input, Select, Text, VStack } from "@chakra-ui/react";
+
+export const FilterForm = () => (
+  <HStack alignItems="end">
+    <VStack>
+      <Text>Start Time</Text>
+      <Input type="datetime-local" onChange={(e) => {}}></Input>
+    </VStack>
+    <VStack>
+      <Text>End Time</Text>
+      <Input type="datetime-local" onChange={(e) => {}}></Input>
+    </VStack>
+    <VStack>
+      <Text>Choose Projects</Text>
+      <>Multi select</>
+    </VStack>
+    <Button variant="solid" onClick={() => {}}>
+      Filter
+    </Button>
+    <VStack>
+      <Text>Frequency</Text>
+      <Select placeholder="Select Frequency" onChange={(e) => {}}>
+        <option value="weekly">Weekly</option>
+        <option value="bi-weekly">Bi Weekly</option>
+        <option value="monthly">Monthly</option>
+        <option value="bi-monthly">Bi Monthly</option>
+      </Select>
+    </VStack>
+  </HStack>
+);
 
 export default FilterForm;
