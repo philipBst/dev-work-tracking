fc2ad53fcc172d120911865aa46d39d06631a6d2
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 12:36:13 +0545

    feat: add multi select input

--- client/src/components/FilterForm.tsx
+++ client/src/components/FilterForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, HStack, Input, Select, Text, VStack } from "@chakra-ui/react";
 import { MultiSelect, Option } from "chakra-multiselect";
 
@@ -16,38 +17,41 @@ export const options: Option[] = [
   },
 ];
 
-export const FilterForm = () => (
-  <HStack alignItems="end">
-    <VStack>
-      <Text>Start Time</Text>
-      <Input type="datetime-local" onChange={(e) => {}}></Input>
-    </VStack>
-    <VStack>
-      <Text>End Time</Text>
-      <Input type="datetime-local" onChange={(e) => {}}></Input>
-    </VStack>
-    <VStack>
-      <Text>Choose Projects</Text>
-      <MultiSelect
-        options={options}
-        value={[]}
-        onChange={(e) => {}}
-        maxWidth={400}
-      />
-    </VStack>
-    <Button variant="solid" onClick={() => {}}>
-      Filter
-    </Button>
-    <VStack>
-      <Text>Frequency</Text>
-      <Select placeholder="Select Frequency" onChange={(e) => {}}>
-        <option value="weekly">Weekly</option>
-        <option value="bi-weekly">Bi Weekly</option>
-        <option value="monthly">Monthly</option>
-        <option value="bi-monthly">Bi Monthly</option>
-      </Select>
-    </VStack>
-  </HStack>
-);
+export const FilterForm = () => {
+  const [selectedItems, setSelectedItems] = useState<string[]>([]);
+  return (
+    <HStack alignItems="end">
+      <VStack>
+        <Text>Start Time</Text>
+        <Input type="datetime-local" onChange={(e) => {}}></Input>
+      </VStack>
+      <VStack>
+        <Text>End Time</Text>
+        <Input type="datetime-local" onChange={(e) => {}}></Input>
+      </VStack>
+      <VStack>
+        <Text>Choose Projects</Text>
+        <MultiSelect
+          options={options}
+          value={selectedItems}
+          onChange={(e) => setSelectedItems(e as string[])}
+          maxWidth={400}
+        />
+      </VStack>
+      <Button variant="solid" onClick={() => {}}>
+        Filter
+      </Button>
+      <VStack>
+        <Text>Frequency</Text>
+        <Select placeholder="Select Frequency" onChange={(e) => {}}>
+          <option value="weekly">Weekly</option>
+          <option value="bi-weekly">Bi Weekly</option>
+          <option value="monthly">Monthly</option>
+          <option value="bi-monthly">Bi Monthly</option>
+        </Select>
+      </VStack>
+    </HStack>
+  );
+};
 
 export default FilterForm;
