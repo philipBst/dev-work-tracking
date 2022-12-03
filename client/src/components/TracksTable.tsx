1a30e6adbf933d39e5b2c2bc591ba5ef73a640c5
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 12:44:40 +0545

    feat: create skeleton table component

---
+++ client/src/components/TracksTable.tsx
@@ -0,0 +1,3 @@
+const TracksTable = () => <>This is table</>;
+
+export default TracksTable;
--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,2 @@
 export { default as FilterForm } from "./FilterForm";
+export { default as TracksTable } from "./TracksTable";
