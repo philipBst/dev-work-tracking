c5b649c9bd93e949789393985a4ca3fd1ba039b7
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 10:49:48 +0545

    feat: add entity for csv data parsing

---
+++ backend/src/entities/tracker.entity.ts
@@ -0,0 +1,10 @@
+export class Entity {
+  activity: string;
+  project: string;
+  workers: string;
+  duration: string;
+  time: string;
+  duration_seconds: number;
+  start_time: string;
+  end_time: string;
+}
