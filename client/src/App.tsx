03a92784170172c9019a1880c163385ee9ffcb97
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 11:59:00 +0545

    feat: apply general layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,12 @@
+import { VStack } from "@chakra-ui/react";
+
 function App() {
-  return <div></div>;
+  return (
+    <VStack>
+      <div>filter input goes here</div>
+      <div>table goes here</div>
+    </VStack>
+  );
 }
 
 export default App;
