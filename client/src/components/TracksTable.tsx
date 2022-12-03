5f42cef13b1b87f0a69b0910b3edc45d68c53a73
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 12:59:31 +0545

    feat: create table component ui

--- client/src/components/TracksTable.tsx
+++ client/src/components/TracksTable.tsx
@@ -1,3 +1,45 @@
-const TracksTable = () => <>This is table</>;
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+
+import { flexRender, HeaderGroup, RowModel } from "@tanstack/react-table";
+
+export type TrackingTableProps = {
+  getHeaderGroups?: () => HeaderGroup<any>[];
+  getRowModel?: () => RowModel<any>;
+};
+
+const TracksTable: React.FC<TrackingTableProps> = ({
+  getHeaderGroups,
+  getRowModel,
+}) => (
+  <Table variant="simple">
+    <Thead>
+      {getHeaderGroups?.().map((headerGroup) => (
+        <Tr key={headerGroup.id}>
+          {headerGroup.headers.map((header) => (
+            <Th key={header.id} colSpan={header.colSpan}>
+              {header.isPlaceholder
+                ? null
+                : flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+            </Th>
+          ))}
+        </Tr>
+      ))}
+    </Thead>
+    <Tbody>
+      {getRowModel?.().rows.map((row) => (
+        <Tr key={row.id}>
+          {row.getVisibleCells().map((cell) => (
+            <Td key={cell.id}>
+              {flexRender(cell.column.columnDef.cell, cell.getContext())}
+            </Td>
+          ))}
+        </Tr>
+      ))}
+    </Tbody>
+  </Table>
+);
 
 export default TracksTable;
