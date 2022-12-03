548c19e3bf14405dbf4188cc8b9a6591817e59b9
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 14:10:55 +0545

    feat: fetch data and pass react table props

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import { VStack } from "@chakra-ui/react";
+import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
 
 import { FilterForm, TracksTable } from "./components";
 
+import { getAllTracks } from "./services/tracks.service";
+
+import { TableData } from "./types";
+
+const preFetch = (() => getAllTracks())();
+
+const initialTableData: TableData = { columns: [], data: [] };
+
 function App() {
+  const [table, setTable] = useState<TableData>(initialTableData);
+
+  const { getHeaderGroups, getRowModel } = useReactTable({
+    columns: table.columns,
+    data: table.data,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  useEffect(() => {
+    preFetch.then((data) => console.log(data));
+  }, []);
+
   return (
     <VStack mt={10}>
       <FilterForm />
-      <TracksTable />
+      <TracksTable
+        getHeaderGroups={getHeaderGroups}
+        getRowModel={getRowModel}
+      />
     </VStack>
   );
 }
