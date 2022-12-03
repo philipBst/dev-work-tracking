3ccc27b07e1772db5b8b46204818d13a219ad99a
Philip Basnet <philipbasnet@gmail.com>
2022-12-03 11:05:40 +0545

    feat: update service to parse csv

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { CsvParser, ParsedData } from 'nest-csv-parser';
+import { TrackerEntity } from './entities/tracker.entity';
 
 @Injectable()
 export class AppService {
-  getAllTracks(): string {
-    return 'Hello World!';
+  constructor(private readonly csvParser: CsvParser) {}
+
+  async getAllTracks() {
+    const stream = createReadStream('./src/store/tracker.csv');
+    const data: ParsedData<TrackerEntity> = await this.csvParser.parse(
+      stream,
+      TrackerEntity,
+      undefined,
+      0,
+      {
+        strict: true,
+        separator: ',',
+      },
+    );
+    return data.list;
   }
 }
